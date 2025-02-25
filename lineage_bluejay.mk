d57427a2c9e7f3b451c300002a58340b951f5b73
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-25 08:59:13 +0800

    bluejay: Ship viperfx
    Change-Id: I2aadead483ebb51258c21177fed3d469a6b7fca0
    (cherry picked from commit 476998c2ab11bd37269109d527b34490e205ab25)

--- lineage_bluejay.mk
+++ lineage_bluejay.mk
@@ -29,6 +29,8 @@ AXION_CAMERA_REAR_INFO := 12.2,12
 AXION_CAMERA_FRONT_INFO := 8
 AXION_MAINTAINER ?= rmp
 
+TARGET_INCLUDE_VIPERFX := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="bluejay-user 16 BP2A.250705.008.A1 13673213 release-keys" \
     BuildFingerprint=google/bluejay/bluejay:16/BP2A.250705.008.A1/13673213:user/release-keys \
